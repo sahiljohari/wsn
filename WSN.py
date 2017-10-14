a0e88442a8b455ecf9ace8675678c85393cad619
sahiljohari <darewithsj@gmail.com>
2017-10-13 22:39:19 -0500

    Added edges for max and min degree nodes

--- WSN.py
+++ WSN.py
@@ -15,11 +15,14 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
+degree = {}
+c_map = []
+
 
 def plot_Square(nodes, avgDeg):
     start = timeit.default_timer()
-    degree = {}
-    c_map = []
+    # degree = {}
+    # c_map = []
     r = np.sqrt(avgDeg / (nodes * np.pi))
 
     coordinates = np.random.rand(nodes, 2)
@@ -40,7 +43,6 @@ def plot_Square(nodes, avgDeg):
     G.add_nodes_from(range(nodes))
     G.add_edges_from(list(pairs))
 
-    # G[max(degree.items(), key=operator.itemgetter(1))[0]]['color'] = 'red'
     pos = dict(zip(range(nodes), coordinates))
     print('\n-----------------------------')
     print('No. of edges: ', len(pairs))
@@ -48,24 +50,38 @@ def plot_Square(nodes, avgDeg):
     print('Min Degree: ', min_deg)
     print('Average Degree: ', observed_avg_deg)
     print('-----------------------------')
-    print('Elapsed time: ', timeit.default_timer() - start, ' second(s)')
+    print('Elapsed time: ', timeit.default_timer() - start, 'second(s)')
+
+    max_edges = []
+    min_edges = []
+    max_node = max(degree.items(), key=operator.itemgetter(1))[0]
+    min_node = min(degree.items(), key=operator.itemgetter(1))[0]
+
+    for i in list(pairs):
+        if i[0] == max_node or i[1] == max_node:
+            max_edges.append(i)
+        if i[0] == min_node or i[1] == min_node:
+            min_edges.append(i)
 
     for node in G:
-        if node == max(degree.items(), key=operator.itemgetter(1))[0]:
+        if node == max_node:
             c_map.append('red')
-        elif node == min(degree.items(), key=operator.itemgetter(1))[0]:
+        elif node == min_node:
             c_map.append('blue')
         else:
-            c_map.append('yellow')
+            c_map.append('black')
 
-    nx.draw(G, pos, node_size=4, node_color=c_map, alpha=0.5)
+    nx.draw(G, pos, node_size=4, node_color=c_map, alpha=0.4, edge_color = '#00916a')
+    nx.draw_networkx_edges(G, pos, edgelist=max_edges, edge_color = 'red', width = 2.0)
+    nx.draw_networkx_edges(G, pos, edgelist=min_edges, edge_color='blue', width=2.0)
     plt.axis('equal')
     plt.show()
 
 
 def plot_Disk(nodes, avgDeg):
     start = timeit.default_timer()
-    degree = {}
+    # degree = {}
+    # c_map = []
     r = np.sqrt(avgDeg / nodes)
 
     theta = np.random.uniform(0, 2 * np.pi, nodes)
@@ -101,7 +117,28 @@ def plot_Disk(nodes, avgDeg):
     print('-----------------------------')
     print('Elapsed time: ', timeit.default_timer() - start)
 
-    nx.draw(G, pos, node_size=4, node_color='red', alpha=0.4)
+    max_edges = []
+    min_edges = []
+    max_node = max(degree.items(), key=operator.itemgetter(1))[0]
+    min_node = min(degree.items(), key=operator.itemgetter(1))[0]
+
+    for i in list(pairs):
+        if i[0] == max_node or i[1] == max_node:
+            max_edges.append(i)
+        if i[0] == min_node or i[1] == min_node:
+            min_edges.append(i)
+
+    for node in G:
+        if node == max_node:
+            c_map.append('red')
+        elif node == min_node:
+            c_map.append('blue')
+        else:
+            c_map.append('black')
+
+    nx.draw(G, pos, node_size=4, node_color=c_map, alpha=0.4, edge_color='#00916a')
+    nx.draw_networkx_edges(G, pos, edgelist=max_edges, edge_color='red', width=2.0)
+    nx.draw_networkx_edges(G, pos, edgelist=min_edges, edge_color='blue', width=2.0)
     plt.axis('equal')
     plt.show()
 
